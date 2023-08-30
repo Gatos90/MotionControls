94da6776f2dc66f9e2912aae447cbf303743e106
kevin.janzen@t-systems.com <kevin.janzen@t-systems.com>
2023-08-30 11:13:44 +0200

    Fast with PYQT

--- gui.py
+++ gui.py
@@ -1,30 +1,63 @@
-# gui.py
-
-import tkinter as tk
-from PIL import Image, ImageTk
-from motion_tracking import videofeed
-import threading
-
-def stream(label):
-    frame = videofeed()
-    if frame is not None:
-        img = Image.fromarray(frame)
-        imgtk = ImageTk.PhotoImage(image=img)
-        label.imgtk = imgtk
-        label.configure(image=imgtk)
-    label.after(10, lambda: stream(label))
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
+import cv2
+import numpy as np
+from motion_tracking import Track_Object
+import time  # Added for FPS calculation
 
 
-root = tk.Tk()
-frame_label = tk.Label(root)
-frame_label.grid(row=0, column=0)  # Position at row 0, column 0
+class ThreadClass(QThread):
+    send_image = pyqtSignal(np.ndarray)
+    FPS = pyqtSignal(int)  # Signal for FPS
 
-text_label = tk.Label(root, text="Hello")
-text_label.grid(row=1, column=1)  # Position at row 1, column 1
+    def run(self):
+        video_capture = cv2.VideoCapture(0)
+        tracker = Track_Object()
+        prev_frame_time = 0
+        new_frame_time = 0
+        while True:
+            _, frame = video_capture.read()
+            tracked_frame = tracker.process_image(frame)
+            
+            # FPS Calculation
+            new_frame_time = time.time()
+            fps = 1 / (new_frame_time - prev_frame_time)
+            prev_frame_time = new_frame_time
+            self.FPS.emit(int(fps))
+            
+            if tracked_frame is not None:
+                self.send_image.emit(tracked_frame)
 
-thread = threading.Thread(target=stream, args=(frame_label,))
-thread.daemon = 1
-thread.start()
 
-root.mainloop()
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.image_label = QLabel(self)
+        layout = QVBoxLayout()
+        layout.addWidget(self.image_label)
+        self.setLayout(layout)
 
+        self.thread = ThreadClass()
+        self.thread.send_image.connect(self.update_image)
+        self.thread.FPS.connect(self.update_fps)  # Connect the FPS signal
+        self.thread.start()
+
+    @pyqtSlot(np.ndarray)
+    def update_image(self, cv_img):
+        height, width, channel = cv_img.shape
+        bytes_per_line = 3 * width
+        qt_image = QImage(cv_img.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
+        self.image_label.setPixmap(QPixmap.fromImage(qt_image))
+
+    @pyqtSlot(int)
+    def update_fps(self, fps):
+        # You can update a label or any other widget with the FPS value here.
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    win = Window()
+    win.show()
+    app.exec_()
--- motion_tracking.py
+++ motion_tracking.py
@@ -1,100 +1,77 @@
+#motion_tracking.py
 import cv2
 import mediapipe as mp
-import tkinter as tk
-from PIL import Image, ImageTk
 
-mp_drawing = mp.solutions.drawing_utils
-mp_holistic = mp.solutions.holistic
+class Track_Object:
+    def __init__(self):
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_holistic = mp.solutions.holistic  # Changed to lowercase 'h'
+        self.holistic = self.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)  # Initializing holistic model here
+        self.prev_y = [None] * 4
+        self.velocity = [0] * 4
+        self.jump_threshold = 0.08
+        self.jump_velocity_threshold = 0.02 
 
-cap = cv2.VideoCapture(0)
-
-prev_y = [None] * 4
-velocity = [0] * 4
-jump_threshold = 0.08  # Set threshold for jump detection
-jump_velocity_threshold = 0.02  # Set threshold for velocity to consider as a jump
-
-window = tk.Tk()
-canvas = tk.Canvas(window, width = cap.get(3), height = cap.get(4))
-canvas.pack()
-
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while True:
-        ret, frame = cap.read()
-
-        # Check if frame was captured successfully
-        if not ret:
-            print("Frame not captured successfully")
-            break
-
+    def process_image(self, frame):      
+        # Initial processing
         image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        results = holistic.process(image)
+        image.flags.writeable = False 
+        
+        results = self.holistic.process(image)   # Use self.holistic instance
 
         if results.pose_landmarks:
             cur_y = []
             landmarks_to_check = [
-                mp_holistic.PoseLandmark.LEFT_SHOULDER,
-                mp_holistic.PoseLandmark.RIGHT_SHOULDER,
-                mp_holistic.PoseLandmark.LEFT_HIP,
-                mp_holistic.PoseLandmark.RIGHT_HIP,
+                self.mp_holistic.PoseLandmark.LEFT_SHOULDER,
+                self.mp_holistic.PoseLandmark.RIGHT_SHOULDER,
+                self.mp_holistic.PoseLandmark.LEFT_HIP,
+                self.mp_holistic.PoseLandmark.RIGHT_HIP,
             ]
 
             for i, landmark in enumerate(landmarks_to_check):
                 y = results.pose_landmarks.landmark[landmark].y
                 cur_y.append(y)
-                if prev_y[i] is not None:
-                    velocity[i] = y - prev_y[i]
 
-            if None not in prev_y:
-                shoulder_change = abs(prev_y[0] - cur_y[0]) + abs(prev_y[1] - cur_y[1])
-                hip_change = abs(prev_y[2] - cur_y[2]) + abs(prev_y[3] - cur_y[3])
+                if self.prev_y[i] is not None:
+                    self.velocity[i] = y - self.prev_y[i]
 
-                shoulder_velocity = velocity[0] + velocity[1]
-                hip_velocity = velocity[2] + velocity[3]
+            if None not in self.prev_y:
+                shoulder_change = abs(self.prev_y[0] - cur_y[0]) + abs(self.prev_y[1] - cur_y[1])
+                hip_change = abs(self.prev_y[2] - cur_y[2]) + abs(self.prev_y[3] - cur_y[3])
+
+                shoulder_velocity = self.velocity[0] + self.velocity[1]
+                hip_velocity = self.velocity[2] + self.velocity[3]
 
                 if (
-                    shoulder_change > jump_threshold
-                    and shoulder_velocity > jump_velocity_threshold
+                    shoulder_change > self.jump_threshold and 
+                    shoulder_velocity > self.jump_velocity_threshold
                 ) and (
-                    hip_change > jump_threshold
-                    and hip_velocity > jump_velocity_threshold
+                    hip_change > self.jump_threshold and 
+                    hip_velocity > self.jump_velocity_threshold
                 ):
-                    print(
-                        f"Jump detected: Shoulder Change={shoulder_change}, Hip Change={hip_change}"
-                    )
+                    print(f"Jump detected: Shoulder Change={shoulder_change}, Hip Change={hip_change}")
 
-            prev_y = cur_y.copy()
-        else:
-            print("Pose landmarks not found")
+            self.prev_y = cur_y.copy()
 
-        image.flags.writeable = True
+        image.flags.writeable = True 
 
-        mp_drawing.draw_landmarks(
-            image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS
+        self.mp_drawing.draw_landmarks(
+            image, results.face_landmarks, self.mp_holistic.FACEMESH_CONTOURS
         )
-        mp_drawing.draw_landmarks(
-            image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS
+        self.mp_drawing.draw_landmarks(
+            image, results.left_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS
         )
-        mp_drawing.draw_landmarks(
-            image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS
+        self.mp_drawing.draw_landmarks(
+            image, results.right_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS
         )
-        mp_drawing.draw_landmarks(
-            image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS
+        self.mp_drawing.draw_landmarks(
+            image, results.pose_landmarks, self.mp_holistic.POSE_CONNECTIONS
         )
-
-        photo = ImageTk.PhotoImage(image = Image.fromarray(image))
-
-        canvas.create_image(0, 0, image = photo, anchor = tk.NW)
-
-        window.update_idletasks()
-        window.update()
 
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        if cv2.waitKey(10) & 0xFF == ord("q"):
-            print("Exit command received, stopping...")
-            break
+        return image  
 
-cap.release()
-cv2.destroyAllWindows()
-print("End of program")
+    # Make sure to clean up the instance when you are done
+    def __del__(self):
+        self.holistic.close()

950ebab91e02014685546b6b6cfa2db13229bbc2
kevin.janzen@t-systems.com <kevin.janzen@t-systems.com>
2023-08-30 08:57:59 +0200

    Test

--- gui.py
+++ gui.py
@@ -1,30 +1,66 @@
 # gui.py
 
-import tkinter as tk
-from PIL import Image, ImageTk
-from motion_tracking import videofeed
-import threading
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import Qt, QTimer
+from motion_tracking import MotionTracker
 
-def stream(label):
-    frame = videofeed()
-    if frame is not None:
-        img = Image.fromarray(frame)
-        imgtk = ImageTk.PhotoImage(image=img)
-        label.imgtk = imgtk
-        label.configure(image=imgtk)
-    label.after(10, lambda: stream(label))
+class VideoWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        # Set the main window to the size of the display
+        screen = QApplication.primaryScreen()
+        screen_size = screen.size()
+        self.setGeometry(0, 0, screen_size.width(), screen_size.height())
 
-root = tk.Tk()
-frame_label = tk.Label(root)
-frame_label.grid(row=0, column=0)  # Position at row 0, column 0
+        # Create a central widget and layout for the main window
+        central_widget = QWidget(self)
+        layout = QVBoxLayout(central_widget)
+        self.setCentralWidget(central_widget)
 
-text_label = tk.Label(root, text="Hello")
-text_label.grid(row=1, column=1)  # Position at row 1, column 1
+        # Create a QLabel to simulate a video display
+        self.video_label = QLabel(self)
+        self.video_label.setFixedSize(300, 300)
+        self.video_label.setAlignment(Qt.AlignCenter)
+        # For demonstration purposes, set a red background to the QLabel
+        self.video_label.setStyleSheet("background-color: red;")
 
-thread = threading.Thread(target=stream, args=(frame_label,))
-thread.daemon = 1
-thread.start()
+        layout.addWidget(self.video_label, alignment=Qt.AlignCenter)
 
-root.mainloop()
+        self.motion_tracker = MotionTracker()
 
+        # Create a QTimer to update the video frame
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(15)  # Update every 30 ms
+
+    def update_frame(self):
+        frame = self.motion_tracker.get_frame()
+        if frame is not None:
+            height, width, channel = frame.shape
+            bytes_per_line = 3 * width
+            q_img = QImage(frame.data, width, height, bytes_per_line, QImage.Format_RGB888)
+            pixmap = QPixmap.fromImage(q_img)
+            self.video_label.setPixmap(pixmap)
+
+    def closeEvent(self, event):
+        self.motion_tracker.release()
+        event.accept()
+
+    def resizeEvent(self, event):
+        # Resize the video_label proportionally with the main window
+        width_ratio = self.width() / self.screen().size().width()
+        height_ratio = self.height() / self.screen().size().height()
+
+        new_width = int(300 * width_ratio)  # Convert to integer
+        new_height = int(300 * height_ratio)   # Convert to integer
+
+        self.video_label.setFixedSize(new_width, new_height)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = VideoWindow()
+    window.show()
+    sys.exit(app.exec_())

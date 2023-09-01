328b40e719b089fd12b704213eebab397865a15f
kevin.janzen@t-systems.com <kevin.janzen@t-systems.com>
2023-09-01 17:30:18 +0200

    AddedMulticlass

--- motion_tracking.py
+++ motion_tracking.py
@@ -13,8 +13,9 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
 import pickle
 
-CSV_PATH = "models/cords/coords.csv"
-MODEL_PATH = "models/models/body_language.pkl"
+CSV_DIR = "models/cords"
+MODEL_DIR = "models/models"
+FEATURE_DIR = "models/features"
 
 
 class Track_Object:
@@ -29,6 +30,7 @@ class Track_Object:
         self.holistic = self.mp_holistic.Holistic(
             min_detection_confidence=0.5, min_tracking_confidence=0.5
         )
+        self.models = self.load_all_models("models/models/")
         self.palm_start_time = None
         self.palm_end_time = None
         self.prev_y = [None] * 4
@@ -36,17 +38,37 @@ class Track_Object:
         self.jump_threshold = 0.08
         self.jump_velocity_threshold = 0.02
         self.saved_positions = None
-        if os.path.exists(MODEL_PATH):
-            with open(MODEL_PATH, "rb") as f:
-                start_time = time.time()
-                self.model = pickle.load(f)
-                print(self.model)
-                end_time = time.time()
-                self.loading_time = end_time - start_time
-                print(f"Model loading time: {self.loading_time} seconds")
-            # Load feature names here
-            with open("models/features/feature_names.txt", "r") as f:
-                self.feature_names = [line.strip() for line in f]
+        self.models = self.load_all_models("models/models/")
+        start_time = time.time()
+        end_time = time.time()
+        self.loading_time = end_time - start_time
+
+        # Load feature names here
+        with open("models/features/feature_names.txt", "r") as f:
+            self.feature_names = [line.strip() for line in f]
+
+    def get_csv_path(self):
+        """Returns the path to the CSV for the current class."""
+        return os.path.join(CSV_DIR, f"coords_{self.current_class_name}.csv")
+
+    def get_model_path(self):
+        """Returns the path to the model for the current class."""
+        return os.path.join(MODEL_DIR, f"body_language_{self.current_class_name}.pkl")
+
+    def get_feature_path(self):
+        """Returns the path to the feature names file for the current class."""
+        return os.path.join(FEATURE_DIR, f"features_{self.current_class_name}.txt")
+
+    def load_all_models(self, directory_path):
+        model_files = [f for f in os.listdir(directory_path) if f.endswith(".pkl")]
+        models = {}
+        for model_file in model_files:
+            with open(os.path.join(directory_path, model_file), "rb") as f:
+                model_name = model_file[
+                    :-4
+                ]  # removing the .pkl extension to get the model's name
+                models[model_name] = pickle.load(f)
+        return models
 
     def draw_landmarks(self, image, landmarks, connections, color1, color2):
         self.mp_drawing.draw_landmarks(
@@ -128,8 +150,11 @@ class Track_Object:
                 "v{}".format(val),
             ]
 
-        if not os.path.exists(CSV_PATH) or os.stat(CSV_PATH).st_size == 0:
-            with open(CSV_PATH, mode="w", newline="") as f:
+        if (
+            not os.path.exists(self.get_csv_path())
+            or os.stat(self.get_csv_path()).st_size == 0
+        ):
+            with open(self.get_csv_path(), mode="w", newline="") as f:
                 csv_writer = csv.writer(
                     f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
                 )
@@ -157,7 +182,7 @@ class Track_Object:
             )
             row = pose_row + face_row
             row.insert(0, class_name)
-            with open(CSV_PATH, mode="a", newline="") as f:
+            with open(self.get_csv_path(), mode="a", newline="") as f:
                 csv_writer = csv.writer(
                     f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
                 )
@@ -182,7 +207,7 @@ class Track_Object:
 
     def train_model(self):
         start_time = time.time()  # Record the start time
-        df = pd.read_csv(CSV_PATH)
+        df = pd.read_csv(self.get_csv_path())
         X = df.drop("class", axis=1)
         y = df["class"]
 
@@ -208,125 +233,125 @@ class Track_Object:
             self.model = pipeline.fit(X_train, y_train)
             fit_models[algo] = self.model
 
-        with open(MODEL_PATH, "wb") as f:
+        with open(self.get_model_path(), "wb") as f:
             pickle.dump(fit_models["rf"], f)
             end_time = time.time()  # Record the end time
-        with open("feature_names.txt", "w") as f:
+        with open(self.get_feature_path(), "w") as f:
             for col in X_train.columns:
                 f.write(col + "\n")
             print(f"The train_model execution time : {end_time - start_time} seconds")
 
     def predict_model(self, image, results):
-        if self.model is None:
-            return
-        pose = results.pose_landmarks.landmark
-        pose_row = list(
-            np.array(
-                [
-                    [landmark.x, landmark.y, landmark.z, landmark.visibility]
-                    for landmark in pose
-                ]
-            ).flatten()
-        )
-        face = results.face_landmarks.landmark
-        face_row = list(
-            np.array(
-                [
-                    [landmark.x, landmark.y, landmark.z, landmark.visibility]
-                    for landmark in face
-                ]
-            ).flatten()
-        )
-
-        row = pose_row + face_row
-        X = pd.DataFrame([row], columns=self.feature_names)
-        body_language_class = self.model.predict(X)[0]
-        body_language_prob = self.model.predict_proba(X)[0]
-        class_index = list(self.model.classes_).index(body_language_class)
-        prob_of_predicted_class = body_language_prob[class_index]
-        if prob_of_predicted_class * 100 > 70:
-            print(
-                f"Predicted Class: {body_language_class}, Probability: {prob_of_predicted_class*100}%"
+        for model_name, model in self.models.items():
+            # You can use the model_name for any labeling or printing needs
+            pose = results.pose_landmarks.landmark
+            pose_row = list(
+                np.array(
+                    [
+                        [landmark.x, landmark.y, landmark.z, landmark.visibility]
+                        for landmark in pose
+                    ]
+                ).flatten()
             )
-            coords = tuple(
-                np.multiply(
-                    np.array(
-                        (
-                            results.pose_landmarks.landmark[
-                                self.mp_holistic.PoseLandmark.LEFT_EAR
-                            ].x,
-                            results.pose_landmarks.landmark[
-                                self.mp_holistic.PoseLandmark.LEFT_EAR
-                            ].y,
-                        )
-                    ),
-                    [640, 480],
-                ).astype(int)
-            )
-            cv2.rectangle(
-                image,
-                (coords[0], coords[1] + 5),
-                (coords[0] + len(body_language_class) * 20, coords[1] - 30),
-                (245, 117, 16),
-                -1,
-            )
-            cv2.putText(
-                image,
-                body_language_class,
-                coords,
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA,
+            face = results.face_landmarks.landmark
+            face_row = list(
+                np.array(
+                    [
+                        [landmark.x, landmark.y, landmark.z, landmark.visibility]
+                        for landmark in face
+                    ]
+                ).flatten()
             )
 
-            # Get status box
-            cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
+            row = pose_row + face_row
+            X = pd.DataFrame([row], columns=self.feature_names)
+            body_language_class = model.predict(X)[0]  # Change here
+            body_language_prob = model.predict_proba(X)[0]  # And here
+            class_index = list(model.classes_).index(body_language_class)
+            prob_of_predicted_class = body_language_prob[class_index]
+            if prob_of_predicted_class * 100 > 70:
+                print(
+                    f"Predicted Class: {body_language_class}, Probability: {prob_of_predicted_class*100}%"
+                )
+                coords = tuple(
+                    np.multiply(
+                        np.array(
+                            (
+                                results.pose_landmarks.landmark[
+                                    self.mp_holistic.PoseLandmark.LEFT_EAR
+                                ].x,
+                                results.pose_landmarks.landmark[
+                                    self.mp_holistic.PoseLandmark.LEFT_EAR
+                                ].y,
+                            )
+                        ),
+                        [640, 480],
+                    ).astype(int)
+                )
+                cv2.rectangle(
+                    image,
+                    (coords[0], coords[1] + 5),
+                    (coords[0] + len(body_language_class) * 20, coords[1] - 30),
+                    (245, 117, 16),
+                    -1,
+                )
+                cv2.putText(
+                    image,
+                    body_language_class,
+                    coords,
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA,
+                )
 
-            # Display Class
-            cv2.putText(
-                image,
-                "CLASS",
-                (95, 12),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (0, 0, 0),
-                1,
-                cv2.LINE_AA,
-            )
-            cv2.putText(
-                image,
-                body_language_class.split(" ")[0],
-                (90, 40),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA,
-            )
+                # Get status box
+                cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
 
-            # Display Probability
-            cv2.putText(
-                image,
-                "PROB",
-                (15, 12),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (0, 0, 0),
-                1,
-                cv2.LINE_AA,
-            )
-            cv2.putText(
-                image,
-                str(round(body_language_prob[np.argmax(body_language_prob)], 2)),
-                (10, 40),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA,
-            )
+                # Display Class
+                cv2.putText(
+                    image,
+                    "CLASS",
+                    (95, 12),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 0, 0),
+                    1,
+                    cv2.LINE_AA,
+                )
+                cv2.putText(
+                    image,
+                    body_language_class.split(" ")[0],
+                    (90, 40),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA,
+                )
+
+                # Display Probability
+                cv2.putText(
+                    image,
+                    "PROB",
+                    (15, 12),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 0, 0),
+                    1,
+                    cv2.LINE_AA,
+                )
+                cv2.putText(
+                    image,
+                    str(round(body_language_prob[np.argmax(body_language_prob)], 2)),
+                    (10, 40),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA,
+                )
 
     def set_class_name(self, class_name):
         """Sets the current class name for exporting."""

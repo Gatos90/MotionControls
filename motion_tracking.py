30ccf897ca10b53a8b0ca5facc5255397889769a
Gatos90 <verweyen.kevin@gmail.com>
2023-09-05 08:22:40 +0200

    Working seperation

--- motion_tracking.py
+++ motion_tracking.py
@@ -94,19 +94,13 @@ class Track_Object:
             )
 
     def get_feature_path(self):
-        """Returns the path to the feature names file for the current class."""
+        """Returns the path to the feature names file based on the current export category."""
         if self.exportingLeftHand:
-            return os.path.join(
-                FEATURE_DIR, f"left_hand_{self.current_class_name}.txt"
-            )
+            return os.path.join(FEATURE_DIR, "left_hand.txt")
         if self.exportingRightHand:
-            return os.path.join(
-                FEATURE_DIR, f"right_hand_{self.current_class_name}.txt"
-            )
+            return os.path.join(FEATURE_DIR, "right_hand.txt")
         if self.exportingBodyPose:
-            return os.path.join(
-                FEATURE_DIR, f"body_pose_{self.current_class_name}.txt"
-            )
+            return os.path.join(FEATURE_DIR, "body_pose.txt")
 
     def load_all_models(self, directory_path):
         model_files = [f for f in os.listdir(directory_path) if f.endswith(".pkl")]
@@ -167,7 +161,7 @@ class Track_Object:
 
         if self.should_train:
             try:
-                self.train_model()
+                self.train_data()
                 self.should_train = False
             except Exception as e:
                 print(f"Error during model training: {e}")
@@ -199,12 +193,13 @@ class Track_Object:
 
         # Get hand landmarks
         if results.right_hand_landmarks:
-            num_coords_hand = len(results.right_hand_landmarks.landmark)
+            print(f"Right hand landmarks data: {results.right_hand_landmarks}")
+            num_coords_hand_right = len(results.right_hand_landmarks.landmark)
         else:
-            num_coords_hand = 0
-        landmarks_hand = ["class"]
-        for val in range(1, num_coords_hand + 1):
-            landmarks_hand += [
+            num_coords_hand_right = 0
+        landmarks_right_hand = ["class"]
+        for val in range(1, num_coords_hand_right + 1):
+            landmarks_right_hand += [
                 "x{}".format(val),
                 "y{}".format(val),
                 "z{}".format(val),
@@ -212,12 +207,12 @@ class Track_Object:
             ]
 
         if results.left_hand_landmarks:
-            num_coords_hand = len(results.left_hand_landmarks.landmark)
+            num_coords_hand_left = len(results.left_hand_landmarks.landmark)
         else:
-            num_coords_hand = 0
-        landmarks_hand = ["class"]
-        for val in range(1, num_coords_hand + 1):
-            landmarks_hand += [
+            num_coords_hand_left = 0
+        landmarks_left_hand = ["class"]
+        for val in range(1, num_coords_hand_left + 1):
+            landmarks_left_hand += [
                 "x{}".format(val),
                 "y{}".format(val),
                 "z{}".format(val),
@@ -251,21 +246,24 @@ class Track_Object:
                         f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
                     )
                     csv_writer.writerow(row_pose)
+
             except Exception as e:
                 print(f"Error exporting coordinates to CSV: {e}")
 
         # Log left hand landmarks if checkbox is checked
         if self.exportingLeftHand and results.left_hand_landmarks:
-            if not os.path.exists(self.get_feature_path()):
-                with open(self.get_feature_path(), mode="w", newline="") as f:
-                    for val in ["class"] + landmarks_hand[1:]:
+            feature_path = self.get_feature_path()
+            if not os.path.exists(feature_path):
+                with open(feature_path, mode="w", newline="") as f:
+                    for val in ["class"] + landmarks_left_hand[1:]:
                         f.write(val + "\n")
             if not os.path.exists(self.get_csv_path_left_hand()):
                 with open(self.get_csv_path_left_hand(), mode="w", newline="") as f:
                     csv_writer = csv.writer(
                         f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
                     )
-                    csv_writer.writerow(landmarks_hand)
+                    csv_writer.writerow(landmarks_left_hand)
+
             try:
                 hand = results.left_hand_landmarks.landmark
                 hand_row = list(
@@ -283,21 +281,25 @@ class Track_Object:
                         f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
                     )
                     csv_writer.writerow(row_hand)
+
             except Exception as e:
                 print(f"Error exporting coordinates to CSV: {e}")
 
         # Log right hand landmarks if checkbox is checked
         if self.exportingRightHand and results.right_hand_landmarks:
-            if not os.path.exists(self.get_feature_path()):
-                with open(self.get_feature_path(), mode="w", newline="") as f:
-                    for val in ["class"] + landmarks_hand[1:]:
+            feature_path = self.get_feature_path()
+            if not os.path.exists(feature_path):
+                with open(feature_path, mode="w", newline="") as f:
+                    for val in ["class"] + landmarks_right_hand[1:]:
+                        print(f"Writing: {val}")
                         f.write(val + "\n")
+                  
             if not os.path.exists(self.get_csv_path_right_hand()):
                 with open(self.get_csv_path_right_hand(), mode="w", newline="") as f:
                     csv_writer = csv.writer(
                         f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
                     )
-                    csv_writer.writerow(landmarks_hand)
+                    csv_writer.writerow(landmarks_right_hand)
 
             try:
                 hand = results.right_hand_landmarks.landmark
@@ -334,44 +336,56 @@ class Track_Object:
     def should_show_video(self):
         self.should_show_image = True
 
-    def train_model(self):
-        start_time = time.time()  # Record the start time
-        if self.exportingRightHand:
-            df = pd.read_csv(self.get_csv_path_right_hand())
+    def train_data(self):
+        category = ""
         if self.exportingLeftHand:
-            df = pd.read_csv(self.get_csv_path_left_hand())
+            category = "left_hand"
+        if self.exportingRightHand:
+            category = "right_hand"
         if self.exportingBodyPose:
-            df = pd.read_csv(self.get_csv_path_pose())
-        X = df.drop("class", axis=1)
-        y = df["class"]
+            category = "body_pose"
+        if category == "":
+            return
+        pattern = f"{category}_{{class}}.txt"
+        all_files = os.listdir(CSV_DIR)
+        relevant_files = [f for f in all_files if f.startswith(category)]
+        combined_data = pd.concat(
+            [pd.read_csv(os.path.join(CSV_DIR, f)) for f in relevant_files],
+            ignore_index=True,
+        )
+        combined_data.to_csv("combined_data.csv", index=False)
+        # Combine data first
 
-        # For one-class classification (anomaly detection)
-        if len(y.unique()) == 1:
-            # Using OneClassSVM for one-class classification
-            model = make_pipeline(
-                StandardScaler(), OneClassSVM(nu=0.1, kernel="rbf", gamma=0.01)
-            )
-            model.fit(X)
-        else:
-            X_train, X_test, y_train, y_test = train_test_split(
-                X, y, test_size=0.3, random_state=1234
-            )
-            # Convert back to DataFrame to retain column names
-            X_train = pd.DataFrame(X_train, columns=X.columns)
-            X_test = pd.DataFrame(X_test, columns=X.columns)
+        # If no data was returned (because no files were found for the category), exit the function
+        if combined_data is None:
+            print(f"Skipping training for category {category} due to lack of data.")
+            return
 
-            # Train a Random Forest model
-            pipeline_rf = make_pipeline(StandardScaler(), RandomForestClassifier())
-            model = pipeline_rf.fit(X_train, y_train)
+        X = combined_data.drop("class", axis=1)
+        y = combined_data["class"]
 
-        with open(self.get_model_path(), "wb") as f:
-            pickle.dump(model, f)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.3, random_state=1234
+        )
+        X_train = pd.DataFrame(X_train, columns=X.columns)
+        X_test = pd.DataFrame(X_test, columns=X.columns)
 
-        end_time = time.time()  # Record the end time
-        print(f"The train_model execution time : {end_time - start_time} seconds")
+        pipelines = {
+            "rf": make_pipeline(StandardScaler(), RandomForestClassifier()),
+        }
+
+        fit_models = {}
+        for algo, pipeline in pipelines.items():
+            model = pipeline.fit(X_train, y_train)
+            fit_models[algo] = model
+
+        for algo_name, trained_model in fit_models.items():
+            model_path = os.path.join(MODEL_DIR, f"{category}.pkl")
+            with open(model_path, "wb") as f:
+                pickle.dump(trained_model, f)
 
     def predict_model(self, image, results):
-        #pdb.set_trace()
+        # pdb.set_trace()
         for model_name, model in self.models.items():
             try:
                 # Pose
@@ -431,18 +445,154 @@ class Track_Object:
                 else:
                     continue
                 X = pd.DataFrame([row], columns=self.feature_names[model_name])
-                
-                y_pred = model.predict(X)
-                if y_pred[0] == 1: 
-                    print(f"A match for {model_name} has been found.")
+                body_language_class = model.predict(X)[0]
+                body_language_prob = model.predict_proba(X)[0]
+                class_index = list(model.classes_).index(body_language_class)
+                prob_of_predicted_class = body_language_prob[class_index]
+                if model_name == "left_hand" and prob_of_predicted_class * 100 > 70:
+                    print(
+                        f"Predicted Class: {body_language_class}, Probability: {prob_of_predicted_class*100}%"
+                    )
+                    coords = tuple(
+                        np.multiply(
+                            np.array(
+                                (
+                                    results.left_hand_landmarks.landmark[0].x,  # Index 0 corresponds to the left wrist in the hand landmarks
+                                    results.left_hand_landmarks.landmark[0].y,
+                                )
+                            ),
+                            [640, 480],
+                        ).astype(int)
+)
 
-            except Exception as e:
-                print(f"Error in model '{model_name}': {e}")
-                traceback.print_exc()      
-        
-            
+                    # Calculate text width and height with the new font size
+                    (text_width, text_height), _ = cv2.getTextSize(body_language_class + str(round(body_language_prob[np.argmax(body_language_prob)], 2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, 1)
+
+                    # Adjust padding if needed. Here, I'm reducing the padding since the font is smaller.
+                    padding = 5
+
+                    # Draw the rectangle based on the new text size
+                    cv2.rectangle(
+                        image,
+                        (coords[0], coords[1] + padding),
+                        (coords[0] + text_width + padding, coords[1] - text_height - padding),
+                        (245, 117, 16),
+                        -1,
+                    )
+
+                    # Draw the text with adjusted font size
+                    cv2.putText(
+                        image,
+                        body_language_class + str(round(body_language_prob[np.argmax(body_language_prob)], 2)),
+                        coords,
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7,  # Adjusted font size to 0.7
+                        (255, 255, 255),
+                        1,  # Adjusted line thickness to 1 for smaller text
+                        cv2.LINE_AA,
+                    )                     
+                   
+
+                if model_name == "right_hand" and prob_of_predicted_class * 100 > 70:
+                        print(
+                            f"Predicted Class: {body_language_class}, Probability: {prob_of_predicted_class*100}%"
+                        )
+                        coords = tuple(
+                            np.multiply(
+                                np.array(
+                                    (
+                                        results.right_hand_landmarks.landmark[0].x,  # Index 0 corresponds to the left wrist in the hand landmarks
+                                        results.right_hand_landmarks.landmark[0].y,
+                                    )
+                                ),
+                                [640, 480],
+                            ).astype(int)
+    )
+
+                        # Calculate text width and height with the new font size
+                        (text_width, text_height), _ = cv2.getTextSize(body_language_class + str(round(body_language_prob[np.argmax(body_language_prob)], 2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, 1)
+
+                        # Adjust padding if needed. Here, I'm reducing the padding since the font is smaller.
+                        padding = 5
+
+                        # Draw the rectangle based on the new text size
+                        cv2.rectangle(
+                            image,
+                            (coords[0], coords[1] + padding),
+                            (coords[0] + text_width + padding, coords[1] - text_height - padding),
+                            (245, 117, 16),
+                            -1,
+                        )
+
+                        # Draw the text with adjusted font size
+                        cv2.putText(
+                            image,
+                            body_language_class + str(round(body_language_prob[np.argmax(body_language_prob)], 2)),
+                            coords,
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7,  # Adjusted font size to 0.7
+                            (255, 255, 255),
+                            1,  # Adjusted line thickness to 1 for smaller text
+                            cv2.LINE_AA,
+                        )
+
+                if model_name == "right_hand" and prob_of_predicted_class * 100 > 70:
+                        print(
+                            f"Predicted Class: {body_language_class}, Probability: {prob_of_predicted_class*100}%"
+                        )
+                        
+                        # Get status box
+                        cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
+
+                        # Display Class
+                        cv2.putText(
+                            image,
+                            "CLASS",
+                            (95, 12),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.5,
+                            (0, 0, 0),
+                            1,
+                            cv2.LINE_AA,
+                        )
+                        cv2.putText(
+                            image,
+                            body_language_class.split(" ")[0],
+                            (90, 40),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1,
+                            (255, 255, 255),
+                            2,
+                            cv2.LINE_AA,
+                        )
+
+                        # Display Probability
+                        cv2.putText(
+                            image,
+                            "PROB",
+                            (15, 12),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.5,
+                            (0, 0, 0),
+                            1,
+                            cv2.LINE_AA,
+                        )
+                        cv2.putText(
+                            image,
+                            str(round(body_language_prob[np.argmax(body_language_prob)], 2)),
+                            (10, 40),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1,
+                            (255, 255, 255),
+                            2,
+                            cv2.LINE_AA,
+                        )
+
                 
-    
+            except Exception as e:
+                    print(f"Error in model '{model_name}': {e}")
+                    traceback.print_exc()
+
     def set_class_name(self, class_name):
         """Sets the current class name for exporting."""
         if class_name:  # Ensure that the class name isn't empty
@@ -474,3 +624,6 @@ class Track_Object:
                 image, (brect[0], brect[1]), (brect[2], brect[3]), (0, 0, 0), 2
             )
         return image
+
+
+# saddasd
